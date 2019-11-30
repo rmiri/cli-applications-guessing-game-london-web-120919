72a763ec0a6c389f120f022f443ce56d39dd11fd
Renata Freitas <r.miriuk@gmail.com>
2019-11-30 17:07:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -13,7 +13,7 @@ def run_guessing_game
   user_i = gets.chomp
     if user_i == "exit"
       return "Goodbye!"
-    elsif user_i == random
+    elsif user_i.to_i == random
       print "you won!"
     else
       print "Sorry! The computer guessed #{random}."
